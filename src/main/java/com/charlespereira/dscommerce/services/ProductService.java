e38c88d579822b1a52949f6a5ce0bef6b9c0bbab
Charles Pereira <charlesmyller@hotmail.com>
2023-05-21 19:58:23 +0200

    PUT Product

--- src/main/java/com/charlespereira/dscommerce/services/ProductService.java
+++ src/main/java/com/charlespereira/dscommerce/services/ProductService.java
@@ -33,12 +33,23 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
        Product entity = new Product();
-       entity.setName(dto.getName());
-       entity.setDescription(dto.getDescription());
-       entity.setPrice(dto.getPrice());
-       entity.setImgUrl(dto.getImgUrl());
-
+       copyToEntity(dto, entity);
        entity = repository.save(entity);
        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        Product entity = repository.getReferenceById(id);
+        copyToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
     }
 }
