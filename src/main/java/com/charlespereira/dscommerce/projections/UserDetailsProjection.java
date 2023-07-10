6e95e20313873a0c6d177949601e7aa0e6877623
Charles Pereira <charlesmyller@hotmail.com>
2023-07-10 19:31:59 +0200

    UserDetailService implemented

---
+++ src/main/java/com/charlespereira/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package com.charlespereira.dscommerce.projections;
+
+public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
